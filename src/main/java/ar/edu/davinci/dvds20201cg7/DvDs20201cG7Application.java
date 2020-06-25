f705868eeff332fade9d30f6c19e4e823e15f85e
frajomon <frajomon@gmail.com>
2020-06-25 00:13:13 -0300

    Agregado del trabajo hecho hasta el lunes 22-06-2020

---
+++ src/main/java/ar/edu/davinci/dvds20201cg7/DvDs20201cG7Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20201cg7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20201cG7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20201cG7Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20201cg7/DvDs20201cG7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20201cg7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20201cG7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
